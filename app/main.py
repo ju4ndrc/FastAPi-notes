cf209640194fa5e6f3da3da5737f33b64c51bb05
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-09-27 19:31:22 -0500

    [+]middleware addeds in main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+import time
+from http.client import responses
+from time import process_time
+
+from fastapi import FastAPI,Request
 from models import Transaction,Invoice
 from db import SessionDep,create_all_tables
 from sqlmodel import select
@@ -10,6 +14,25 @@ app.include_router(customers.router)
 app.include_router(transactions.router)
 app.include_router(plans.router , tags=["Plans"], prefix="/Plans")
 
+@app.middleware("http")
+async def log_request_time(requets:Request,call_next):
+    stat_time = time.time()
+
+    response = await call_next(requets) #aqui se define un middleware basico
+
+    process_time = time.time() - stat_time #aqui se calcula cuamto tardo en ejecutarse response = await call_next(requets)
+
+    print(f"Request: {requets.url} completed in : {process_time:.4f} seconds")
+
+    return response # se retorna una respuesta por que se esta interceptando un request
+
+@app.middleware("http")
+async def get_headers(requets: Request, call_next):
+    print("Requests Headers")
+    for header , value in requets.headers.items():
+        print(f"{header}:{value}")
+    response = await call_next(requets)
+    return response
 @app.get("/")
 async def home():
     return{"Hello":"Juan🤑"}
